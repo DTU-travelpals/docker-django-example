ef4d97f86881ab863c22be6e2fbc615775acb89d
DTU-travelpals <daniel.tomsu@travelpals.de>
2025-09-16 21:37:25 +0200

    removed some initial stuff

--- src/pages/tasks.py
+++ src/pages/tasks.py
@@ -6,4 +6,4 @@ def add_name_to_queue(name):
     # For now, we'll just print the name to the console.
     # In a real application, you might do something more complex here.
     print(f"Received name: {name}")
-    return f"Hello, {name}!"
+    return f"{name}"
